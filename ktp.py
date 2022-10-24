08c193fbf07b4c22cbfc91b3843364fd6e847392
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-10-24 20:13:36 +0800

    Add files via upload

---
+++ ktp.py
@@ -0,0 +1,7 @@
+#kecukupan umur untuk membuat ktp
+usia = int(input("masukkan umur anda (dalam bentuk angka :"))
+
+if usia >= 17 :
+    print("umur anda telah memenuhi syarat untuk pembuatan ktp")
+else:
+    print("mohon maaf umur anda belum cukup untuk membuat ktp")
